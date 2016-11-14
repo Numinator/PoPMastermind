970d2209060901ae220d496aa3eb7051d96a9e0e
Aiyu Liu <quack@Aiyus-MacBook-Pro.local>
2016-11-14 17:52:03 +0100

    Tilføjer færdigt program

--- Source/8g0a.fsx
+++ Source/8g0a.fsx
@@ -1,6 +1,5 @@
 (* The types *)
-type codeColor =
-  |Red | Green | Yellow | Purple | White | Black
+type codeColor = Red | Green | Yellow | Purple | White | Black
 
 type code = codeColor list
 
@@ -9,14 +8,16 @@ type answer = int * int
 type board = ( code * answer ) list
 
 type player = Human | Computer
+let mutable gameOver = false
+
 
 (* Menu: askPlayerType *)
 let rec askPlayerType () =
     printfn "Do you want to play as Human (H/h) or Computer (C/c)?"
     let p = (System.Console.ReadLine ()).ToLower ()
-    if p = "h" or p = "human" then
+    if p = "h" || p = "human" then
         Human
-    elif p = "c" or p = "computer" then
+    elif p = "c" || p = "computer" then
         Computer
     else
         printfn "Wrong input"
@@ -33,9 +34,11 @@ let numToCol n =
     | 5 -> Black
 
 (* getRdnCode: Helper function to makeCode *)
-let getRndCode ():code =
+let getRndCode ():code=
     let rnd = System.Random()
-    [for i in 1..4 -> numToCol <| rnd.Next () % 6]
+    let list = [numToCol (rnd.Next() % 6);numToCol (rnd.Next() % 6);numToCol (rnd.Next() % 6);numToCol (rnd.Next() % 6);]
+//    [fun for _ in 1..4 -> (numToCol (rnd.Next() % 6))]
+    list
 
 (* stringToCol: Helper function to selCode *)
 let stringToCol s =
@@ -49,11 +52,11 @@ let stringToCol s =
     | _                          -> None
 
 (* selCode: Helper function to makeCode *)
-let rec selCode () =
+let selCode () =
     printfn "Possible colors: Red (r) | Green (g) | Yellow (y) | Purple (p) | White (w) | Black (b)"
     let colors = ["first color";"second color";"third color"; "fourth color"]
     let rec inputHelper i : code=
-      if i > 4 then [] 
+      if i > 3 then [] 
       else
         printfn "Input your %s" colors.[i]
         let c1 = (System.Console.ReadLine ()).ToLower ()
@@ -61,7 +64,7 @@ let rec selCode () =
         | None -> inputHelper i
         | Some(c)    -> printfn "You have chosen %A" c;
                         [c] @ inputHelper (i+1)
-    inputHelper 1
+    inputHelper 0
 
 (*  Game: makeCode*)
 let makeCode (p:player) =
@@ -70,48 +73,45 @@ let makeCode (p:player) =
   | Computer -> getRndCode ()
 
 (* Game: guess *)
-
 //guess skal modificeres til at kunne gøre noget med board - evt. foretage et bedre valg.
+//evt. printBoard
 let guess (p:player) (brd:board) : code =
   match p with
   | Human     -> selCode ()
   | Computer  -> getRndCode ()
 
-let realCode = makeCode (askPlayerType ())
 (* validate: Part of the guess loop  *)
 let validate (rc: code) (c: code) :answer =
   let mutable i   = 0
   let mutable b   = 0
   let mutable w   = 0
-  let listCode    = [Red;Green;Yellow;Purple;White;Black] //en smule unødvendig
   let histC       = [|0;0;0;0;0;0|]
   let histRc      = [|0;0;0;0;0;0|]
   let mutable sum = 0
   for i=0 to 3 do 
     if rc.[i] = c.[i] then
       b <- b + 1
-  match rc.[i] with
-  | col when col = listCode.[0] -> histRc.[0]+1
-  | col when col = listCode.[1] -> histRc.[1]+1
-  | col when col = listCode.[2] -> histRc.[2]+1
-  | col when col = listCode.[3] -> histRc.[3]+1
-  | col when col = listCode.[4] -> histRc.[4]+1
-  | col when col = listCode.[5] -> histRc.[5]+1
     
-  match c.[i] with
-  | col when col = listCode.[0] -> histC.[0]+1
-  | col when col = listCode.[1] -> histC.[1]+1
-  | col when col = listCode.[2] -> histC.[2]+1
-  | col when col = listCode.[3] -> histC.[3]+1
-  | col when col = listCode.[4] -> histC.[4]+1
-  | col when col = listCode.[5] -> histC.[5]+1 
-  
-  i <- i + 1
+    match rc.[i] with
+    | col when col = Red    -> histRc.[0]+1
+    | col when col = Green  -> histRc.[1]+1
+    | col when col = Yellow -> histRc.[2]+1
+    | col when col = Purple -> histRc.[3]+1
+    | col when col = White  -> histRc.[4]+1
+    | col when col = Black  -> histRc.[5]+1
+    
+    match c.[i] with
+    | col when col = Red    -> histC.[0]+1
+    | col when col = Green  -> histC.[1]+1
+    | col when col = Yellow -> histC.[2]+1
+    | col when col = Purple -> histC.[3]+1
+    | col when col = White  -> histC.[4]+1
+    | col when col = Black  -> histC.[5]+1 
   
   for i=0 to 5 do
     sum <- sum + (min (histC.[i]) (histRc.[i]))
-
-  w <- sum - b
+  
+  w <- sum
   (b, w)
       
 
@@ -145,12 +145,18 @@ let printBoard (brd: board) =
 (* isGameOver: Part of the guess loop *)
 let isGameOver (a: answer) =
   if a = (4,0) then
-    true
-  else 
-    false
+    gameOver <- true 
 
 let main () = 
-  makeCode ()
-
-
+    printfn "%A" list
+    let p = askPlayerType ()
+    let realCode = makeCode (p)
+    let mutable board = []
+    
+    while not(gameOver) do 
+      let currentGuess = guess (p) board
+      let answer = validate (realCode) (currentGuess)
+      board <- addGuess board currentGuess answer
+      isGameOver (answer)
+      printBoard (board)
 main ()
